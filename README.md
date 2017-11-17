4316917d421bdd9604799795dda6dc9599c3c787
Anush S Kumar <anushkumar27@gmail.com>
2017-11-17 17:50:26 +0530

    Database final

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NoWaitAppointments
+type = 0; // User => default
+type = 1; // Service Provider
+uid is saved as cookie
+time is in railway timings (GMT +5:30)
+limit is for the online platform, is mentioned in per day
--- nowaitappointments.sql
+++ nowaitappointments.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2017 at 06:34 AM
+-- Generation Time: Nov 17, 2017 at 09:14 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `NoWaitAppointment`
+-- Database: `nowaitappointments`
 --
 
 -- --------------------------------------------------------
@@ -32,9 +32,9 @@ CREATE TABLE `appointment` (
   `aid` int(65) NOT NULL,
   `requestId` int(65) NOT NULL,
   `serviceId` int(65) NOT NULL,
-  `aTime` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `status` int(1) NOT NULL,
-  `closure` int(1) NOT NULL
+  `appTime` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `status` int(1) NOT NULL DEFAULT '0',
+  `closure` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -46,8 +46,20 @@ CREATE TABLE `appointment` (
 CREATE TABLE `category` (
   `categoryId` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `desc` varchar(500) NOT NULL
+  `description` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryId`, `name`, `description`) VALUES
+(1, 'Carpenter', 'Carpenter for all types of wood work'),
+(2, 'Painter', 'Painter for painting'),
+(3, 'Doctor', 'Doctors for All types of patients'),
+(4, 'Plumber', 'Plumber for tap works'),
+(5, 'Consultant', 'Consultant for any problem'),
+(6, 'Laywer', 'Help clients');
 
 -- --------------------------------------------------------
 
@@ -57,15 +69,27 @@ CREATE TABLE `category` (
 
 CREATE TABLE `serviceprofile` (
   `categoryId` int(10) NOT NULL,
-  `currentLat` decimal(15,15) NOT NULL,
-  `currentLong` decimal(15,15) NOT NULL,
+  `currentLat` varchar(30) NOT NULL,
+  `currentLong` varchar(30) NOT NULL,
   `uid` int(65) NOT NULL,
   `price` int(65) NOT NULL,
   `limit` int(65) NOT NULL,
-  `durationStart` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `durationStop` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
+  `durationStart` time(6) NOT NULL,
+  `durationStop` time(6) NOT NULL,
   `rating` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `serviceprofile`
+--
+
+INSERT INTO `serviceprofile` (`categoryId`, `currentLat`, `currentLong`, `uid`, `price`, `limit`, `durationStart`, `durationStop`, `rating`) VALUES
+(1, '12.9376048', '77.5346939', 1, 280, 5, '09:30:00.000000', '17:00:00.000000', 3),
+(2, '12.9323747', '77.5706254', 2, 350, 4, '08:15:00.000000', '17:30:00.000000', 4),
+(3, '12.9419154', '77.5451603', 3, 420, 7, '09:45:00.000000', '16:00:00.000000', 5),
+(4, '12.9394849', '77.5101414', 4, 620, 8, '10:30:00.000000', '18:00:00.000000', 4),
+(5, '12.9396475', '77.5101414', 5, 290, 2, '07:45:00.000000', '15:00:00.000000', 3),
+(6, '12.9354351', '77.5345509', 6, 1400, 9, '08:45:00.000000', '17:15:00.000000', 2);
 
 -- --------------------------------------------------------
 
@@ -74,11 +98,22 @@ CREATE TABLE `serviceprofile` (
 --
 
 CREATE TABLE `userprofile` (
-  `dob` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `dob` date NOT NULL,
   `uid` int(65) NOT NULL,
-  `currentLat` decimal(15,15) NOT NULL,
-  `currentLong` decimal(15,15) NOT NULL
+  `currentLat` varchar(30) NOT NULL,
+  `currentLong` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userprofile`
+--
+
+INSERT INTO `userprofile` (`dob`, `uid`, `currentLat`, `currentLong`) VALUES
+('1999-08-18', 1, '12.974831', '77.60935'),
+('1996-01-08', 2, '12.927607', '77.617035'),
+('1998-11-27', 3, '12.969096', '77.746124'),
+('1995-05-31', 4, '13.097536', '77.595749'),
+('1997-02-07', 5, '13.239945', '77.711792');
 
 -- --------------------------------------------------------
 
@@ -94,6 +129,23 @@ CREATE TABLE `users` (
   `password` varchar(65) NOT NULL,
   `address` varchar(65) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`name`, `uname`, `uid`, `type`, `password`, `address`) VALUES
+('Service A', 'service1', 1, 1, 'service1', 'bangalore'),
+('Service B', 'service2', 2, 1, 'service2', 'bangalore'),
+('Service C', 'service3', 3, 1, 'service3', 'bangalore'),
+('Service D', 'service4', 4, 1, 'service4', 'bangalore'),
+('Service E', 'service5', 5, 1, 'service5', 'bangalore'),
+('Service F', 'service6', 6, 1, 'service6', 'bangalore'),
+('Raaghav', 'user1', 11, 0, 'user1', 'Promont apartments'),
+('Ravi', 'user2', 12, 0, 'user2', 'Skyline Ambrosia Apartment'),
+('Anujay', 'user3', 13, 0, 'user3', 'Sterling Terraces Apartment'),
+('Amit', 'user4', 14, 0, 'user4', 'Farm No 36, Chattarpur Farms'),
+('Subham', 'user5', 15, 0, 'user5', 'Gulmohar Villa');
 
 --
 -- Indexes for dumped tables
@@ -146,12 +198,12 @@ ALTER TABLE `appointment`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `categoryId` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `categoryId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(65) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(65) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- Constraints for dumped tables
 --
